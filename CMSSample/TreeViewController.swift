9e27b915d5a096aa71e8e505b8bd305683c5226d
“MariMurotanigit config --global user.name “MariMurotani <murotanimari@MurotaniMari-no-MacBook-Air.local>
2015-07-11 15:42:32 +0900

    デザイン完了

---
+++ CMSSample/TreeViewController.swift
@@ -0,0 +1,13 @@
+//
+//  TreeViewController.swift
+//  CMSSample
+//
+//  Created by MurotaniMari on 2015/07/11.
+//  Copyright © 2015年 MurotaniMari. All rights reserved.
+//
+
+import Cocoa
+
+class TreeViewController: NSTreeController {
+
+}
---
+++ CMSSample/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  CMSSample
+//
+//  Created by MurotaniMari on 2015/07/11.
+//  Copyright © 2015年 MurotaniMari. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
